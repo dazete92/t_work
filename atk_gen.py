881037e7da19ade49ece6c1a01ca4a9112abe41d
dazete92 <teno3eric@yahoo.com>
2016-02-05 20:24:25 -0800

    sorted attacks by rank

--- atk_gen.py
+++ atk_gen.py
@@ -1,16 +1,26 @@
 import sys
 import time
 import subprocess
+from operator import itemgetter
 from subprocess import Popen, PIPE, STDOUT
 from collections import defaultdict
 
+'''
 def print_attacks(attacks):
    
    for host in attacks:
       for port in attacks[host]:
          for attack in range(len(attacks[host][port])):
             print port, attacks[host][port][attack]['name'], attacks[host][port][attack]['modRank']
+'''
+def print_attacks(attacks):
+   
+   for host in attacks:
+      for attack in range(len(attacks[host])):
+         print attacks[host][attack]['name'], attacks[host][attack]['modRank']
 
+           
+'''
 def determineAttackVectors(db_e, db_s, db_h):
    # figures out attack vectors based on hosts and services
 
@@ -32,8 +42,33 @@ def determineAttackVectors(db_e, db_s, db_h):
                      db[host][service['port']].append(db_e[service['port']][exploit])
                   if db_e[service['port']][exploit]['os'] == "multi":
                      db[host][service['port']].append(db_e[service['port']][exploit])
-               
+   print db         
    return db
+'''
+def determineAttackVectors(db_e, db_s, db_h):
+   # figures out attack vectors based on hosts and services
+
+   db = defaultdict()
+   
+   for host in db_h:
+      db[host] = []
+      services_list = db_s[host]
+      for service in services_list:
+         if service['state'] == "open":
+            if service['port'] in db_e:
+               for exploit in range(len(db_e[service['port']])):
+                  if db_e[service['port']][exploit]['os'] == db_h[host]['os_name']:
+                     db[host].append(db_e[service['port']][exploit])
+                  if db_h[host]['os_name'] == "linux" and db_e[service['port']][exploit]['os'] == "unix":
+                     db[host].append(db_e[service['port']][exploit])
+                  if db_e[service['port']][exploit]['os'] == "multi":
+                     db[host].append(db_e[service['port']][exploit])
+   
+   for host in db_h:
+      db[host] = sorted(db[host], key=itemgetter('rankNum'), reverse=True)
+             
+   return db
+
 '''
 def generate_attacks(attacks, db_h):
    
